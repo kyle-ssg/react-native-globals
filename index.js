ea3ec007cdd6a11b5fdf6df848e65d57c770ffde
kyle <kyle@solidstategroup.com>
2018-03-20 23:20:58 +0000

    Neaten up imports

--- index.js
+++ index.js
@@ -2,31 +2,17 @@
 global.React = require('react');
 global.ReactNative = require('react-native');
 global.React.PropTypes = require("prop-types");
+
 //React native components
 global.ActivityIndicator = ReactNative.ActivityIndicator;
-global.Animated = ReactNative.Animated;
-global.AppRegistry = ReactNative.AppRegistry;
-global.AppState = ReactNative.AppState;
-global.AsyncStorage = ReactNative.AsyncStorage;
 global.Button = ReactNative.Button;
 global.DataSource = ReactNative.ListView.DataSource;
-global.DeviceEventEmitter = ReactNative.DeviceEventEmitter;
-global.Dimensions = ReactNative.Dimensions;
-global.Easing = ReactNative.Easing;
 global.FlatList = ReactNative.FlatList;
 global.Image = ReactNative.Image;
-global.InteractionManager = ReactNative.InteractionManager;
 global.KeyboardAvoidingView  = ReactNative.KeyboardAvoidingView;
-global.Linking = ReactNative.Linking;
 global.ListView = ReactNative.ListView;
 global.Modal = ReactNative.Modal;
-global.NativeAppEventEmitter = ReactNative.NativeAppEventEmitter;
-global.NativeModules = ReactNative.NativeModules;
-global.NetInfo = ReactNative.NetInfo;
-global.PanResponder = ReactNative.PanResponder;
 global.Picker = ReactNative.Picker;
-global.PixelRatio = ReactNative.PixelRatio;
-global.Platform = ReactNative.Platform;
 global.RefreshControl = ReactNative.RefreshControl;
 global.ScrollView = ReactNative.ScrollView;
 global.SectionList = ReactNative.SectionList;
@@ -37,26 +23,16 @@ global.Switch = ReactNative.Switch;
 global.Text = ReactNative.Text;
 global.TextInput = ReactNative.TextInput;
 global.TouchableHighlight = ReactNative.TouchableHighlight;
-global.TouchableOpacity = ReactNative.TouchableOpacity;
-global.TouchableHighlight = ReactNative.TouchableHighlight;
 global.TouchableNativeFeedback = ReactNative.TouchableNativeFeedback;
+global.TouchableOpacity = ReactNative.TouchableOpacity;
 global.TouchableWithoutFeedback = ReactNative.TouchableWithoutFeedback;
 global.View = ReactNative.View;
 global.VirtualizedList = ReactNative.VirtualizedList;
 global.WebView = ReactNative.WebView;
-global.WindowedListView = ReactNative.WindowedListView;
 
 
 // APIS
-global.WebView = ReactNative.WebView;
-
-//Device info
-global.DeviceHeight = Dimensions.get("window").height;
-global.DeviceWidth = Dimensions.get("window").width;
-
-
-
-
+global.AccessibilityInfo = ReactNative.AccessibilityInfo;
 global.Alert = ReactNative.Alert;
 global.Animated = ReactNative.Animated;
 global.AppRegistry = ReactNative.AppRegistry;
@@ -65,6 +41,7 @@ global.AsyncStorage = ReactNative.AsyncStorage;
 global.BackHandler = ReactNative.BackHandler;
 global.CameraRoll = ReactNative.CameraRoll;
 global.Clipboard = ReactNative.Clipboard;
+global.DeviceEventEmitter = ReactNative.DeviceEventEmitter;
 global.Dimensions = ReactNative.Dimensions;
 global.Easing = ReactNative.Easing;
 global.ImageEditor = ReactNative.ImageEditor;
@@ -73,12 +50,21 @@ global.InteractionManager = ReactNative.InteractionManager;
 global.Keyboard = ReactNative.Keyboard;
 global.LayoutAnimation = ReactNative.LayoutAnimation;
 global.Linking = ReactNative.Linking;
+global.NativeAppEventEmitter = ReactNative.NativeAppEventEmitter;
 global.NativeMethodsMixin = ReactNative.NativeMethodsMixin;
+global.NativeModules = ReactNative.NativeModules;
 global.NetInfo = ReactNative.NetInfo;
 global.PanResponder = ReactNative.PanResponder;
 global.PixelRatio = ReactNative.PixelRatio;
+global.Platform = ReactNative.Platform;
 global.Settings = ReactNative.Settings;
 global.Share = ReactNative.Share;
 global.StyleSheet = ReactNative.StyleSheet;
 global.Systrace = ReactNative.Systrace;
 global.Vibration = ReactNative.Vibration;
+global.WebView = ReactNative.WebView;
+
+//Device info
+global.DeviceHeight = Dimensions.get("window").height;
+global.DeviceWidth = Dimensions.get("window").width;
+
