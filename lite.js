b99dec51f222dac29f758dedc271d84b4a7c8711
Kyle Johnson <kyle@solidstategroup.com>
2019-10-30 14:20:40 +0000

    Bump version, add lite

---
+++ lite.js
@@ -0,0 +1,32 @@
+//Parent globals
+import ReactNative from 'react-native';
+
+//React native components
+global.ActivityIndicator = ReactNative.ActivityIndicator;
+global.Button = ReactNative.Button;
+global.FlatList = ReactNative.FlatList;
+global.Image = ReactNative.Image;
+global.ImageBackground = ReactNative.ImageBackground;
+global.KeyboardAvoidingView  = ReactNative.KeyboardAvoidingView;
+global.Modal = ReactNative.Modal;
+global.ScrollView = ReactNative.ScrollView;
+global.SectionList = ReactNative.SectionList;
+global.StyleSheet = ReactNative.StyleSheet;
+global.Switch = ReactNative.Switch;
+global.Text = ReactNative.Text;
+global.TextInput = ReactNative.TextInput;
+global.TouchableOpacity = ReactNative.TouchableOpacity;
+global.View = ReactNative.View;
+
+// APIS
+global.Alert = ReactNative.Alert;
+global.Clipboard = ReactNative.Clipboard;
+global.DeviceEventEmitter = ReactNative.DeviceEventEmitter;
+global.Linking = ReactNative.Linking;
+global.PixelRatio = ReactNative.PixelRatio;
+global.Platform = ReactNative.Platform;
+
+//Device info
+global.DeviceHeight = ReactNative.Dimensions.get("window").height;
+global.DeviceWidth = ReactNative.Dimensions.get("window").width;
+
