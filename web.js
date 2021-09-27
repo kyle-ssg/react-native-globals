f1d038689c3203486575cbbf1aee1a5f7e181d54
kyle-ssg <kyle@solidstategroup.com>
2021-09-27 14:32:37 +0100

    support web

---
+++ web.js
@@ -0,0 +1,59 @@
+//Parent globals
+global.React = require('react');
+global.ReactNative = require('react-native-web');
+global.React.PropTypes = require("prop-types");
+
+//React native components
+global.ActivityIndicator = ReactNative.ActivityIndicator;
+global.Button = ReactNative.Button;
+global.FlatList = ReactNative.FlatList;
+global.Image = ReactNative.Image;
+global.ImageBackground = ReactNative.ImageBackground;
+global.KeyboardAvoidingView  = ReactNative.KeyboardAvoidingView;
+global.Modal = ReactNative.Modal;
+global.RefreshControl = ReactNative.RefreshControl;
+global.SafeAreaView = ReactNative.SafeAreaView;
+global.ScrollView = ReactNative.ScrollView;
+global.SectionList = ReactNative.SectionList;
+global.StatusBar = ReactNative.StatusBar;
+global.StyleSheet = ReactNative.StyleSheet;
+global.Switch = ReactNative.Switch;
+global.Text = ReactNative.Text;
+global.TextInput = ReactNative.TextInput;
+global.TouchableHighlight = ReactNative.TouchableHighlight;
+global.TouchableNativeFeedback = ReactNative.TouchableNativeFeedback;
+global.TouchableOpacity = ReactNative.TouchableOpacity;
+global.TouchableWithoutFeedback = ReactNative.TouchableWithoutFeedback;
+global.View = ReactNative.View;
+global.VirtualizedList = ReactNative.VirtualizedList;
+
+// APIS
+global.AccessibilityInfo = ReactNative.AccessibilityInfo;
+global.Alert = ReactNative.Alert;
+global.Animated = ReactNative.Animated;
+global.AppRegistry = ReactNative.AppRegistry;
+global.AppState = ReactNative.AppState;
+global.BackHandler = ReactNative.BackHandler;
+global.DeviceEventEmitter = ReactNative.DeviceEventEmitter;
+global.Dimensions = ReactNative.Dimensions;
+global.Easing = ReactNative.Easing;
+global.InteractionManager = ReactNative.InteractionManager;
+global.Keyboard = ReactNative.Keyboard;
+global.LayoutAnimation = ReactNative.LayoutAnimation;
+global.Linking = ReactNative.Linking;
+global.NativeAppEventEmitter = ReactNative.NativeAppEventEmitter;
+global.NativeMethodsMixin = ReactNative.NativeMethodsMixin;
+global.NativeModules = ReactNative.NativeModules;
+global.PanResponder = ReactNative.PanResponder;
+global.PixelRatio = ReactNative.PixelRatio;
+global.Platform = ReactNative.Platform;
+global.Settings = ReactNative.Settings;
+global.Share = ReactNative.Share;
+global.StyleSheet = ReactNative.StyleSheet;
+global.Systrace = ReactNative.Systrace;
+global.Vibration = ReactNative.Vibration;
+
+//Device info
+global.DeviceHeight = Dimensions.get("window").height;
+global.DeviceWidth = Dimensions.get("window").width;
+
